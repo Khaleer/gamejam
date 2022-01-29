488a89cd891047f6eea705ee3010457d493abe16
Khaleer <hubkro002@tutanota.com>
2022-01-29 16:30:04 +0100

    popup is showing and closing lol

--- game/levels/popup.gd
+++ game/levels/popup.gd
@@ -7,3 +7,9 @@ func _ready():
 func _on_items2_popup_flower() -> void:
 	$MarginContainer.visible = true
 	$MarginContainer/MarginContainer/Label.text = "marchew piękna jest kochana jest marchew piękna jest kochana jest marchew piękna jest kochana jest marchew piękna jest kochana jest "
+	$Timer.start()
+	
+
+
+func _on_Timer_timeout() -> void:
+	$MarginContainer.visible = false
