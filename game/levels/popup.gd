1364c006c22e2f7fcd1d8c9e6c3cc56969c2635f
Khaleer <hubkro002@tutanota.com>
2022-01-29 16:23:00 +0100

    managing stuff

--- game/levels/popup.gd
+++ game/levels/popup.gd
@@ -1,3 +1,9 @@
-extends Control
+extends CanvasLayer
+
+func _ready():
+	$MarginContainer.visible = false
 
 
+func _on_items2_popup_flower() -> void:
+	$MarginContainer.visible = true
+	$MarginContainer/MarginContainer/Label.text = "marchew piękna jest kochana jest marchew piękna jest kochana jest marchew piękna jest kochana jest marchew piękna jest kochana jest "
