9a36f15cb8aadfc37ddb20c8b55231bf49fa7268
Khaleer <hubkro002@tutanota.com>
2022-01-29 02:05:55 +0100

    UI, item system and time machine
    Item needs to be found to start time machine yay!

--- game/items/Time_machine.gd
+++ game/items/Time_machine.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_Time_machine_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and Global.item_one_collected == true:
 		get_tree().change_scene("res://levels/Level_2.tscn")
