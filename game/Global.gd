c3ebc98824699dbc23064d694bda2dac4ebde1ac
Khaleer <hubkro002@tutanota.com>
2022-01-30 08:42:11 +0100

    kwiat i cebula

--- game/Global.gd
+++ game/Global.gd
@@ -3,5 +3,5 @@ extends Node
 var item_one_collected = false
 var item_onion_collected = false
 var item_flower_collected = false
-var item_four_collected = false
+var item_lightbulb_collected = false
 var item_five_collected = false
