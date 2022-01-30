c73666dca2bdd9da602cf93b28ea76b1c1adef30
Khaleer <hubkro002@tutanota.com>
2022-01-30 11:04:15 +0100

    Music and farmer moaning

--- game/levels/Level_1.gd
+++ game/levels/Level_1.gd
@@ -10,7 +10,7 @@ extends Node2D
 func _ready() -> void:
 	$transition_Screen/AnimationPlayer.play("fade_to_transparent")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _physics_process(delta):
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
+	pass
